a2e9ac6206da7325dda446cc2689c4ff43501097
Alfat1989 <gilmalf89@gmail.com>
2021-09-23 18:50:29 +0500

    Добавляет папку js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var names = ['Alfat'];
+// names.push('Alfat');
+// names.push('Alfrit');
+// names.push('Dina');
+// names.push('Farit');
+
+if (names === 'Alfat') {
+    console.log('Hello me!!!');
+} else {
+    console.log('Is not');
+}

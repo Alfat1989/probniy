1d04974706461eec39a7e36e2ca00896924bb9eb
Alfat1989 <gilmalf89@gmail.com>
2021-09-26 02:57:06 +0500

    Рандомность

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-var colors = [];
-colors.push('red');
-colors.push('green');
-colors.push('blue');
-colors.push('black');
-colors.unshift('yellow')
+var weather = ['rain', 'snow', 'hail', 'money is poured'];
 
-console.log(colors);
-// alert("Массив: "+colors)
-
-var fruits = ['qiwi', 'bannana', 'apple', 'orange', 'limon'];
-console.log(fruits)
-
-var box = ['ananas', 'tomato', 'potato'];
-var pack = fruits.concat(box)
-console.log(pack)
+console.log(weather[Math.floor(Math.random()*4)])

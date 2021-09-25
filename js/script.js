1c5966c9df854a84ce43b7617c5430a699200fa1
Alfat1989 <gilmalf89@gmail.com>
2021-09-26 02:51:02 +0500

    Конкатинация

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@ colors.push('red');
 colors.push('green');
 colors.push('blue');
 colors.push('black');
+colors.unshift('yellow')
 
-console.log(colors[2]);
+console.log(colors);
+// alert("Массив: "+colors)
+
+var fruits = ['qiwi', 'bannana', 'apple', 'orange', 'limon'];
+console.log(fruits)
+
+var box = ['ananas', 'tomato', 'potato'];
+var pack = fruits.concat(box)
+console.log(pack)
